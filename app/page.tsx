ca0f52e073c53114a60189b0dea51557b6992b0b
b33fb0n3 <b33fb0n3yt@gmail.com>
2024-12-12 21:32:16 +0100

    added links to specifc days

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import {BiWorld} from "react-icons/bi";
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { BiWorld } from 'react-icons/bi';
 
 export default function Home() {
-    return (
-        <>
-            <h1 className={"text-2xl flex items-center gap-x-2"}>
-                <BiWorld className={"w-6 h-6"}/>
-                Hello World!
-            </h1>
-        </>
-    );
+	return (
+		<>
+			<h1 className={'text-2xl flex items-center'}>
+				<BiWorld className={'w-6 h-6'} />
+				Hello World!
+			</h1>
+			<div className={'flex flex-col gap-y-2'}>
+				{[...Array(24)].map((e, i) => {
+					const day = i + 1;
+
+					return (
+						<Link
+							href={`/day/${day}`}
+							className={cn(buttonVariants({ variant: 'link' }), 'w-fit')}
+							key={day}>
+							Day {day}
+						</Link>
+					);
+				})}
+			</div>
+		</>
+	);
 }
